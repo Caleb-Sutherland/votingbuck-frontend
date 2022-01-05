4b751533d068857396a3d9169d87b8f228ed2ba2
Caleb <csuthe42@uwo.ca>
2022-01-05 16:31:46 -0500

    testing linter

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      	<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      	</header>
     </div>
   );
 }

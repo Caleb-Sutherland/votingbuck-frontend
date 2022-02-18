b11262756c1e607d0bf93a3ee662af63affb43d9
jackhillier-gl <jahill@live.ca>
2022-02-17 20:35:51 -0500

    Minor responsivity upgrades

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="bg-teal-500 h-20 flex items-center">
       <div className="pl-12 text-white flex item-center">
-        <Link className="text-3xl font-bold" to="/">Website</Link>
+        <Link className="text-3xl font-bold" to="/">Voting Buck</Link>
         <div className="ml-12 pl-12 flex items-center">
           <Link className="ml-12 text-xl font-medium text-opacity-75" to="organizations">
             Organizations

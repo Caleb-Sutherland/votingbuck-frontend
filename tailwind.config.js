2881ec160f92c8add9baa325e281a533e4cbd47d
Caleb <csuthe42@uwo.ca>
2022-03-01 11:08:53 -0500

    add custom color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      democratic: "#0015BC",
+      republican: "#FF0000",
+      other: "#22c74e",
+    },
     extend: {},
   },
   plugins: [],
-}
+};

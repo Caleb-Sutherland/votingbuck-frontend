414d39fbaae1e1bedfb9bb08d08a301ce54b8e1f
Jack <jahill@live.ca>
2022-03-02 13:26:07 -0500

    Added gradient background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-teal-500 h-20 flex items-center">
+    <header className="h-20 flex items-center absolute">
       <div className="pl-12 text-white flex item-center">
         <Link className="text-3xl font-bold" to="/">Voting Buck</Link>
         <div className="ml-12 pl-12 flex items-center">
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,34 +2,35 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div className="flex flex-col space-y-24 py-48 px-20">
-      <div className="flex flex-col space-y-5 max-w-5xl mx-auto">
-        <p className="text-4xl sm:text-5xl font-medium text-center">View political donation information for organizations and political figures.</p>  
-        <p className="text-2xl sm:text-3xl font-light text-center">Research and discover political associations through donation information.</p>
-        <div className="flex flex-row space-x-2 justify-center">
-          <input type="text" placeholder="Organization or Individual" className="w-56 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring"/>
-          <button className="bg-purple-500 text-white active:bg-purple-600 font-regular text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150" type="button">
-            Search
-          </button>
+    <div>
+      <div className="flex flex-col space-y-24 py-48 px-20 bg-gradient-to-tr from-red-500 to-blue-500 h-screen">
+        <div className="flex flex-col space-y-5 max-w-5xl m-auto">
+          <p className="text-4xl sm:text-5xl font-medium text-center text-white">View political donation information for organizations and political figures.</p>  
+          <p className="text-2xl sm:text-3xl font-light text-center text-white">Research and discover political associations through donation information.</p>
+          <div className="flex flex-row space-x-2 justify-center">
+            <input type="text" placeholder="Organization or Individual" className="w-72 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring"/>
+          </div>
         </div>
       </div>
-      <div className="-m-5 flex flex-row flex-wrap justify-center">
-        <div className="m-5 w-96 relative flex flex-col overflow-hidden bg-white shadow-lg rounded-lg">
-          {/*<div className="relative h-40 overflow-hidden">
-            <img className="absolute my-auto" src="https://images.unsplash.com/photo-1554469384-e58fac16e23a"></img>
+      <div className="flex flex-col space-y-24 py-24 px-20">
+        <div className="-m-5 flex flex-row flex-wrap justify-center">
+          <div className="m-5 w-96 relative flex flex-col overflow-hidden bg-white shadow-lg rounded-lg">
+            {/*<div className="relative h-40 overflow-hidden">
+              <img className="absolute my-auto" src="https://images.unsplash.com/photo-1554469384-e58fac16e23a"></img>
+            </div>
+            */}<div className="p-8 flex flex-col space-y-3">
+              <p className="text-lg font-medium">Organizations</p>
+              <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
+  Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+            </div>
           </div>
-          */}<div className="p-8 flex flex-col space-y-3">
-            <p className="text-lg font-medium">Organizations</p>
-            <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-          </div>
-        </div>
-        <div className="m-5 w-96 relative flex flex-col overflow-hidden bg-white shadow-lg rounded-lg">
-          {/*<img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"></img>
-          */}<div className="p-8 flex flex-col space-y-3">
-            <p className="text-lg font-medium">Individuals</p>
-            <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+          <div className="m-5 w-96 relative flex flex-col overflow-hidden bg-white shadow-lg rounded-lg">
+            {/*<img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"></img>
+            */}<div className="p-8 flex flex-col space-y-3">
+              <p className="text-lg font-medium">Individuals</p>
+              <p className="font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
+  Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+            </div>
           </div>
         </div>
       </div>

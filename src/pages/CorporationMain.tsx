b77b6bc06743c7d190a5b844adda40a66bf3cffa
Jack Hillier <jahill@live.ca>
2022-04-03 22:52:33 -0400

    Made content screen height

--- src/pages/CorporationMain.tsx
+++ src/pages/CorporationMain.tsx
@@ -155,7 +155,7 @@ export default function CorporationMain() {
   return (
     <div>
       <Header />
-      <div className="m-20 flex flex-col space-y-5">
+      <div className="h-screen m-20 flex flex-col space-y-5">
         <p className="text-xl lg:text-4xl font-bold">Corporations</p>
         <div className="flex flex-col lg:flex-row-reverse items-center">
           <div className="flex flex-row items-center">
--- src/pages/UniversityMain.tsx
+++ src/pages/UniversityMain.tsx
@@ -80,7 +80,7 @@ export default function UniversityMain() {
   return (
     <div>
       <Header />
-      <div className="m-20 flex flex-col space-y-5">
+      <div className="h-screen m-20 flex flex-col space-y-5">
         <p className="text-xl lg:text-4xl font-bold">Universities</p>
         <div className="flex flex-col lg:flex-row-reverse items-center">
           <div className="flex flex-row items-center">

4a89fa7911dd7e37ef38229fd6ce3e252dacd995
Caleb <csuthe42@uwo.ca>
2022-04-07 22:53:29 -0400

    Fix bus on orderedList

--- src/components/OrderedList.tsx
+++ src/components/OrderedList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
-import { GrDown } from "react-icons/gr";
 import { Link } from "react-router-dom";
 
 export function OrderedList(props: any) {
@@ -11,28 +11,27 @@ export function OrderedList(props: any) {
   const getResult = async () => {
     let url = `${process.env.REACT_APP_API_BASE_URL}/${props.page}/list?sortField=${props.sort.field}&order=${props.sort.order}`;
     if (props.filters.length > 0) {
-      if (props.page == "organizations") {
-        url = url + `&industries=${props.filters.join(',').replace(/&/g, "%26")}`
-      }
-      else if (props.page == "universities") {
-        url = url + `&states=${props.filters.join(',')}`
-      }
-      else if (props.page == "recipients") {
-        url = url + `&states=${props.filters.join(',')}`
+      if (props.page === "organizations") {
+        url =
+          url + `&industries=${props.filters.join(",").replace(/&/g, "%26")}`;
+      } else if (props.page === "universities") {
+        url = url + `&states=${props.filters.join(",")}`;
+      } else if (props.page === "recipients") {
+        url = url + `&states=${props.filters.join(",")}`;
       }
     }
-    const res = await fetch(
-      url
-    );
-    console.log(res);
+    const res = await fetch(url);
     const data = await res.json();
     setResults(data[props.page]);
   };
 
   useEffect(() => {
-    if (results[0] == "empty" || sort != props.sort || filters != props.filters) {
+    if (
+      results[0] === "empty" ||
+      sort !== props.sort ||
+      filters !== props.filters
+    ) {
       setSort(props.sort);
-      console.log(sort);
       setFilters(props.filters);
       getResult();
     }
@@ -47,8 +46,7 @@ export function OrderedList(props: any) {
           <div className="grow-1 basis-1/3" />
         </div>
       );
-    }
-    else if (props.page == "universities") {
+    } else if (props.page == "universities") {
       return (
         <div className="flex flex-row p-4">
           <p className="grow-1 basis-1/4 font-semibold">Name</p>
@@ -57,8 +55,7 @@ export function OrderedList(props: any) {
           <div className="grow-1 basis-1/4" />
         </div>
       );
-    }
-    else if (props.page == "recipients") {
+    } else if (props.page == "recipients") {
       return (
         <div className="flex flex-row p-4">
           <p className="grow-1 basis-1/3 font-semibold">Name</p>
@@ -82,8 +79,7 @@ export function OrderedList(props: any) {
           </div>
         </Link>
       );
-    }
-    else if (props.page == "universities") {
+    } else if (props.page == "universities") {
       return (
         <Link key={index} to={"/universities/" + entry.id}>
           <div className="flex flex-row justify-between bg-white shadow-lg rounded-lg p-4">
@@ -96,8 +92,7 @@ export function OrderedList(props: any) {
           </div>
         </Link>
       );
-    }
-    else if (props.page == "recipients") {
+    } else if (props.page == "recipients") {
       return (
         <Link key={index} to={"/politicians/" + entry.id}>
           <div className="flex flex-row justify-between bg-white shadow-lg rounded-lg p-4">
@@ -112,14 +107,21 @@ export function OrderedList(props: any) {
     }
   };
 
-  return (
-    <div className="relative">
-      {listHeader()}
-      <div className="flex flex-col space-y-4">
-        {results.map((entry: any, index: number) => {
-          return listEntry(entry, index);
-        })}
+  if (results[0] !== "empty") {
+    return (
+      <div className="relative">
+        {listHeader()}
+        <div className="flex flex-col space-y-4">
+          {results.map((entry: any, index: number) => {
+            return listEntry(entry, index);
+          })}
+        </div>
       </div>
+    );
+  }
+  return (
+    <div className="h-screen flex justify-center content-center items-center justify-items-center">
+      <FaSpinner size={50} className="animate-spin mb-16" />
     </div>
   );
 }

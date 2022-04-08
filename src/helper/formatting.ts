d600e78773330ca9d198dc6bed39d8ceb2d0d670
Caleb <csuthe42@uwo.ca>
2022-04-07 23:35:58 -0400

    Small bug fix

--- src/helper/formatting.ts
+++ src/helper/formatting.ts
@@ -8,14 +8,18 @@ export const formatNumber = (num: number) => {
 };
 
 export const formatDollarValueToString = (num: number) => {
-  const suffixes = ["", "k", "m", "b", "t"];
-  const suffixNum = Math.floor(("" + num).length / 3);
-  const shortValue = parseFloat(
-    (suffixNum != 0 ? num / Math.pow(1000, suffixNum) : num).toPrecision(2)
-  );
-  if (shortValue % 1 != 0) {
-    const temp = shortValue.toFixed(1);
-    return temp + suffixes[suffixNum];
+  try {
+    const suffixes = ["", "k", "m", "b", "t"];
+    const suffixNum = Math.floor(("" + num).length / 3);
+    const shortValue = parseFloat(
+      (suffixNum != 0 ? num / Math.pow(1000, suffixNum) : num).toPrecision(2)
+    );
+    if (shortValue % 1 != 0) {
+      const temp = shortValue.toFixed(1);
+      return temp + suffixes[suffixNum];
+    }
+    return shortValue + suffixes[suffixNum];
+  } catch {
+    return num;
   }
-  return shortValue + suffixes[suffixNum];
 };

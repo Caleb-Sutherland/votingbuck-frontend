378eeab58c953bc6568ab5ee38c1d6f18b9f4ee1
Jack <jahill@live.ca>
2022-04-13 16:18:34 -0400

    Search results navigated with arrow keys

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,8 +3,42 @@ import { Link } from "react-router-dom";
 
 export function SearchBar() {
   const [queryResults, setQueryResults] = useState<any>(null);
+  const [cursor, setCursor] = useState<number>(-1);
+  const [cursorLinkPath, setCursorLinkPath] = useState<string>("");
+
+  const handleKeyDown = (e: any) => {
+    if (e.keyCode === 38) {
+      e.preventDefault();
+      setCursor(
+        cursor < 0
+          ? queryResults.politicians.length +
+              queryResults.corporates.length +
+              queryResults.universities.length -
+              1
+          : cursor - 1
+      );
+    } else if (e.keyCode === 40) {
+      e.preventDefault();
+      setCursor(
+        cursor ==
+          queryResults.politicians.length +
+            queryResults.corporates.length +
+            queryResults.universities.length -
+            1
+          ? -1
+          : cursor + 1
+      );
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+      if (cursorLinkPath != "") {
+        window.open(cursorLinkPath, "_self");
+      }
+    }
+  };
 
   const handleChange = async (event: any) => {
+    setCursor(-1);
+    setCursorLinkPath("");
     event.target.value = event.target.value.replace(/[^a-zA-Z0-9 ]/g, "");
     const res = await fetch(
       `${process.env.REACT_APP_API_BASE_URL}/search/${event.target.value}`
@@ -62,6 +96,8 @@ export function SearchBar() {
       <input
         type="text"
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        onClick={() => setCursor(-1)}
         placeholder="corporations, universities or politicians"
         className="w-72 px-3 py-3 placeholder-blueGray300 text-blueGray600 relative bg-white bg-white text-sm border border-blueGray300 outline-none focus:outline-none focus:ring shadow-lg rounded-lg"
       />
@@ -73,9 +109,21 @@ export function SearchBar() {
             </div>
           ) : null}
           {queryResults.politicians.map((item: any, index: number) => {
+            if (
+              cursor == index &&
+              cursorLinkPath != "/politicians/" + item.value.id
+            ) {
+              setCursorLinkPath("/politicians/" + item.value.id);
+            }
             return (
               <Link key={index} to={"/politicians/" + item.value.id}>
-                <div className="pl-8 pr-3 py-1 hover:bg-lightGray transition-colors">
+                <div
+                  className={
+                    "pl-8 pr-3 py-1 transition-colors " +
+                    (cursor == index ? "bg-neutral-300" : "")
+                  }
+                  onMouseEnter={() => setCursor(index)}
+                >
                   <p>{item.value.name}</p>
                 </div>
               </Link>
@@ -87,9 +135,25 @@ export function SearchBar() {
             </div>
           ) : null}
           {queryResults.corporates.map((item: any, index: number) => {
+            if (
+              cursor == queryResults.politicians.length + index &&
+              cursorLinkPath != "/corporations/" + item.value.id
+            ) {
+              setCursorLinkPath("/corporations/" + item.value.id);
+            }
             return (
               <Link key={index} to={"/corporations/" + item.value.id}>
-                <div className="pl-8 pr-3 py-1 hover:bg-lightGray transition-colors">
+                <div
+                  className={
+                    "pl-8 pr-3 py-1 transition-colors " +
+                    (cursor == queryResults.politicians.length + index
+                      ? "bg-neutral-300"
+                      : "")
+                  }
+                  onMouseEnter={() =>
+                    setCursor(queryResults.politicians.length + index)
+                  }
+                >
                   <p>{item.value.name}</p>
                 </div>
               </Link>
@@ -101,9 +165,35 @@ export function SearchBar() {
             </div>
           ) : null}
           {queryResults.universities.map((item: any, index: number) => {
+            if (
+              cursor ==
+                queryResults.politicians.length +
+                  queryResults.corporates.length +
+                  index &&
+              cursorLinkPath != "/universities/" + item.value.id
+            ) {
+              setCursorLinkPath("/universities/" + item.value.id);
+            }
             return (
               <Link key={index} to={"/universities/" + item.value.id}>
-                <div className="pl-8 pr-3 py-1 hover:bg-lightGray transition-colors">
+                <div
+                  className={
+                    "pl-8 pr-3 py-1 transition-colors " +
+                    (cursor ==
+                    queryResults.politicians.length +
+                      queryResults.corporates.length +
+                      index
+                      ? "bg-neutral-300"
+                      : "")
+                  }
+                  onMouseEnter={() =>
+                    setCursor(
+                      queryResults.politicians.length +
+                        queryResults.corporates.length +
+                        index
+                    )
+                  }
+                >
                   <p>{item.value.name}</p>
                 </div>
               </Link>
